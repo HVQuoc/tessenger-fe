5836da57329fb64cf7758b66c277011f641b8069
Quoc Hoang <hoangvanquoc923@gmail.com>
2025-01-19 16:24:57 +0700

    implement login session: /profile onload user context

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,15 +1,14 @@
-import React, { useContext } from 'react'
-import { UserContext } from './UserContext'
+import React, { useContext, useEffect } from "react";
+import { UserContext } from "./UserContext";
+import Register from "./Register";
 
 const Routes = () => {
-    const {username, id} = useContext(UserContext)
-  
-  if (username) return <p>Logged in!</p>
-    return (
-    <div>
-      
-    </div>
-  )
-}
+  const { username, id } = useContext(UserContext);
 
-export default Routes
+  if (username) return <p>Logged in! + {username}</p>;
+  return <div>
+    <Register />
+  </div>;
+};
+
+export default Routes;
